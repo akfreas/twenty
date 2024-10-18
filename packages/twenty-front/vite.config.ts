91f55e5dbab75f6cdb359039cd9c7407964f443b
Alexander Freas <akfreas@gmail.com>
2024-10-18 10:25:11 +0200

    Add chakra, reactmarkdown and update aliases

--- packages/twenty-front/vite.config.ts
+++ packages/twenty-front/vite.config.ts
@@ -131,6 +131,7 @@ export default defineConfig(({ command, mode }) => {
     },
     resolve: {
       alias: {
+        react: path.resolve('../../node_modules/react'),
         path: 'rollup-plugin-node-polyfills/polyfills/path',
       },
     },
